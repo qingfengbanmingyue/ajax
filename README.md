16c1b8153434e0fd443816ff42517a2efc7626e4
青衫 <949958166@qq.com>
2022-06-18 11:37:12 +0800

    更新了里面的单独ajax方法

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 # 首先 
 ## 实例化对象
-```
+```js
    let axios = new Axios({
-    BASR_URL: 一个url地址
+    BASR_URL: 协议+一个域名地址+端口号
     })
 ```
 ### GET方法
-```
-  axios.get('一个地址', {
+```js
+  axios.get('一个api路径', {
       responseType: 'json'
     }, function (res) {
       console.log(res)
@@ -20,8 +20,8 @@
 
 ### POST方法
 
-```
- axios.post("一个地址", {
+```js
+ axios.post("一个api路径", {
       responseType: 'json',
       params: {
         //参数键值对
@@ -30,6 +30,59 @@
       }
 
     }, function (res) {
+      console.log(res)
+    })
+```
+
+### AJAX方法
+
+#### AJAX GET方法
+```js
+      axios.ajax({
+        type:'get',
+        url:'一个api路径', 
+        success(res) {
+        console.log(res);
+      }})
+```
+#### AJAX POST方法
+```js
+      axios.ajax({
+        type:'post',
+        url:'一个api路径', 
+        params: {
+            //参数键值对
+            //例如
+            name:'xxx'
+          }
+        },
+        success(res) {
+        console.log(res)
+        })
+```
+
+#### AJAX DELETE方法
+```js
+      axios.ajax({
+        type:'delete'
+        url:'一个api路径', 
+        success(res) {
+        console.log(res);
+      }})
+```
+
+#### AJAX PUT方法
+```js
+   axios.ajax({
+      type:'put'
+      url:'一个api路径', 
+      params: {
+        //参数键值对
+        //例如
+        name:'xxx'
+      }
+     },
+      success(res) {
       console.log(res)
     })
 ```
--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ class Axios{
   }
   
   get(url, options, callback) {
-    if (arguments.length == 2 && type(arguments[1] == "Function")) {
+    if (arguments.length == 2 && this.#type(arguments[1] == "Function")) {
       callback = arguments[1]
     }
     let responseType = this.responseType
@@ -103,111 +103,142 @@ class Axios{
     ajax.send(query_params)
   }
 
- 
+  ajax(obj) {
+    if (!this.#type(obj)) throw Error(`obj is not defined`)
+    
+   
+    if (Object.is(obj.type.toUpperCase(), `GET`)) {
+   
+      if (Reflect.has(obj, `url`)  && Reflect.has(obj, `success`)) {
+     
+      this.get(obj.url, obj.success)
+      } else {
+        throw Error('error')
+    }
+   
+    } else if (Object.is(obj.type.toUpperCase(), `POST`)) {
+     
+      if (Reflect.has(obj, `url`) && Reflect.has(obj, `params`)&&  Reflect.has(obj, `callback`)) {
+        this.post(obj.url, obj.options, obj.callback)
+      }else {
+        throw Error('error')
+    }
+    } else if (Object.is(obj.type.toUpperCase(), `DELETE`)) {
+        console.log(obj);
+      if (Reflect.has(obj, `url`) ) {
+        console.log(obj);
+        if (arguments.length == 2 && this.#type(arguments[1] == "Function")) {
+          callback = arguments[1]
+        }
+        let responseType = this.responseType
+        let query_params = "?"
+       obj.url = this.BASR_URL + obj.url
+        if (this.#type(arguments[1]) == "Object") {
+          if ('responseType' in options) responseType = options.responseType
+      
+          if ('params' in options) {
+            let params = options.params
+            for (let i in params) {
+              query_params += i + '=' + params[i]
+            }
+            query_params = query_params.substring(0, query_params.length - 1)
+            url += query_params
+          }
+      
+      
+        }
+        console.log(obj.params);
+        let ajax = new XMLHttpRequest()
+          console.log(obj.url);
+        ajax.open("DELETE", obj.url)
+        ajax.onreadystatechange = ()=> {
+          if (ajax.readyState == 4) {
+            if (ajax.status == 200 || ajax.status == 201 || ajax.status == 304) {
+              if (this.responseType == 'json') {
+                callback(JSON.parse(ajax.responseText))
+             
+              } else {
+                callback(ajax.responseText)
+              }
+            }
+          }
+        }
+        ajax.send(query_params)
+      
 
+      }else {
+        throw Error('error')
+    }
+    } else if (Object.is(obj.type.toUpperCase(), `PUT`)) {
+    
+      if (Reflect.has(obj, `url`) && Reflect.has(obj, `type`) && Reflect.has(obj, `params`) && Reflect.has(obj, `success`)) {
+  
+       
+        let responseType = this.responseType
+        let query_params = ''
+        obj.url = this.BASR_URL + obj.url
+        // console.log(obj);
+        if (this.#type(obj.params) == "Object") {
+       
+          if ('responseType' in obj.params) responseType = obj.params.responseType
+        
+          if ('params' in obj) {
+            let params = obj.params
+           
+            if (this.#type(params) == 'Object') {
+              for (let i in params) {
+                query_params += i + '=' + obj.params[i] + '&'
+               
+    
+              }
+              query_params = query_params.substring(0, query_params.length - 1)
+              
+           
+            }
+          }
+    
+        }
+    
+    
+        let ajax = new XMLHttpRequest()
+       
+        let _ = this
+        ajax.open("PUT", obj.url)
+        ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        ajax.onreadystatechange = function () {
+          if (ajax.readyState == 4) {
+            if (ajax.status == 200 || ajax.status == 201 || ajax.status == 304) {
+              if (_.responseType == 'json') {
+                obj.success(JSON.parse(ajax.responseText))
 
-
-
+              } else {
+               obj.success(ajax.responseText)
+              }
+            }
+          }
+        }
+      
+        ajax.send(query_params)
+       
+       
+      } else {
+      throw Error('错了')
+      }
+    }else {
+      throw Error('error')
+  }
 }
+    
+  }
 
 
 
 
 
-//ES5
-
-// function Axios(options) {
-//   if (type(options) != "Object") {
-//     throw Error('有问题')
-//   }
-//   this.BASR_URL = options.BASR_URL
-//   this.type = 'GET';
-//   this.responseType = 'json'
-// }
-// Axios.prototype.post = function (url, options, callback) {
-
-//   if (arguments.length == 2 && type(arguments[1] == "Function")) {
-//     callback = arguments[1]
-//   }
-//   let responseType = this.responseType
-//   let query_params = ''
-//   url = this.BASR_URL + url
-//   if (type(arguments[1]) == "Object") {
-//     if ('responseType' in options) responseType = options.responseType
-
-//     if ('params' in options) {
-//       let params = options.params
-//       if (type(options) == 'Object') {
-//         for (let i in params) {
-//           query_params += i + '=' + params[i] + '&'
-
-
-//         }
-//         query_params = query_params.substring(0, query_params.length - 1)
-
-//         console.log(query_params)
-//       }
-//     }
-
-//   }
-
-
-//   let ajax = new XMLHttpRequest()
-
-//   let _ = this
-//   ajax.open("POST", url)
-//   ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-//   ajax.onreadystatechange = function () {
-//     if (ajax.readyState == 4) {
-//       if (ajax.status == 200 || ajax.status == 201 || ajax.status == 304) {
-//         if (_.responseType == 'json') {
-//           callback(JSON.parse(ajax.responseText))
-//         } else {
-//           callback(ajax.responseText)
-//         }
-//       }
-//     }
-//   }
-//   ajax.send(query_params)
 
 
 
-// }
-// Axios.prototype.get = function (url, options, callback) {
-//   if (arguments.length == 2 && type(arguments[1] == "Function")) {
-//     callback = arguments[1]
-//   }
-//   let responseType = this.responseType
-//   let query_params = "?"
-//   url = this.BASR_URL + url
-//   if (type(arguments[1]) == "Object") {
-//     if ('responseType' in options) responseType = options.responseType
-
-//     if ('params' in options) {
-//       let params = options.params
-//       for (let i in params) {
-//         query_params += i + '=' + params[i]
-//       }
-//       query_params = query_params.substring(0, query_params.length - 1)
-//       url += query_params
-//     }
 
 
-//   }
 
-//   let ajax = new XMLHttpRequest()
-//   let _ = this
-//   ajax.open("GET", url)
-//   ajax.onreadystatechange = function () {
-//     if (ajax.readyState == 4) {
-//       if (ajax.status == 200 || ajax.status == 201 || ajax.status == 304) {
-//         if (_.responseType == 'json') {
-//           callback(JSON.parse(ajax.responseText))
-//         } else {
-//           callback(ajax.responseText)
-//         }
-//       }
-//     }
-//   }
-//   ajax.send()
-// }
+
